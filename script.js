d4ae18cd2c19adba585f71fd7d976a8fdba905e6
Mv <mv@mvaop.ir>
2024-03-05 23:40:19 +0330

    dumb stuf idk

--- script.js
+++ script.js
@@ -1,22 +1,17 @@
-// Function to update the clock with Tehran's local time (without seconds)
 const updateClock = () => {
-    // Get the current date and time in Tehran's time zone
     const tehranTime = new Date().toLocaleTimeString('en-US', {
       timeZone: 'Asia/Tehran',
-      hour12: false, // Use 24-hour format
-      hour: '2-digit', // Display hours in 2-digit format
-      minute: '2-digit' // Display minutes in 2-digit format
+      hour12: false,
+      hour: '2-digit',
+      minute: '2-digit' 
     });
   
-    // Update the clock display with Tehran's local time (including "Time :")
     const clockElement = document.getElementById('clock');
     clockElement.textContent = '  ' + tehranTime;
   };
   
-  // Update the clock every minute (60,000 milliseconds)
   setInterval(updateClock, 60000);
   
-  // Initial call to display the clock immediately
   updateClock();
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -25,7 +20,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const volumeSlider = document.getElementById('volumeSlider');
 
     audio.addEventListener('ended', function() {
-        playBtn.textContent = 'Play'; // Change button text to 'Play' when music ends
+        playBtn.textContent = 'Play'; 
     });
 
     playBtn.addEventListener('click', toggleAudioPlayback);
@@ -47,30 +42,11 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 document.addEventListener('DOMContentLoaded', function() {
-    // Create an audio element for the music
     const audio = new Audio('files/bfm.mp3');
 });
 
 document.addEventListener('DOMContentLoaded', function() {
     let lastTimestamp = 0;
-
-    // document.addEventListener('mousemove', function(e) {
-    //     const currentTimestamp = Date.now();
-    //     if (currentTimestamp - lastTimestamp > 100) {
-    //         lastTimestamp = currentTimestamp;
-    //         requestAnimationFrame(() => createSparkle(e.pageX, e.pageY));
-    //     }
-    // });
-
-    // Track scrolling to update mouse trail position
-    // window.addEventListener('scroll', function() {
-    //     const sparkles = document.querySelectorAll('.sparkle');
-    //     sparkles.forEach(sparkle => {
-    //         const rect = sparkle.getBoundingClientRect();
-    //         sparkle.style.left = rect.left + window.scrollX + 'px';
-    //         sparkle.style.top = rect.top + window.scrollY + 'px';
-    //     });
-    // });
 
     // Made By Kyuiki @ 2024 - kyuiki.com
 
@@ -93,7 +69,6 @@ document.addEventListener("mousemove",(e)=>{
   
   el.style.top = tttemp[1]-(i*((ttemp[1]-e.clientY)/10))+"px"
   el.style.left = tttemp[0]-(i*((ttemp[0]-e.clientX)/10))+"px"
-//   el.innerHTML = "Penis"
 
   i+=1;
     }, 1000/60
